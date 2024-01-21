93d81a835d5005129d22153f18a1040c35f5b5ba
Anand_Shukla <anandshuklagii@gmail.com>
2024-01-21 23:15:23 +0530

    some Changes

--- index.js
+++ index.js
@@ -6,42 +6,64 @@ let displayValue = document.querySelector(".enteredNumber span p");
 let aside = document.querySelector(".aside");
 let para = document.createElement("p");
 let para1 = document.createElement("p");
+const X_mark = document.querySelector(".fa-xmark");
 let para2 = document.createElement("p");
 const alerts = document.querySelector(".alert");
 let i = 1;
-const number = Math.trunc(Math.random() * 100);
+const number = Math.trunc(Math.random() * 300) + 1;
 displayValue.innerHTML = `?`;
+// console.log(X_mark);
+let run = true;
 const increment = () => {
-  let values = formValue.value;
-  if (values > number) {
-    aside.appendChild(
-      para
-    ).innerHTML = `You Entered <span> ${values} </span> is Greater than the actual number `;
-  } else if (values < number) {
-    aside.appendChild(
-      para
-    ).innerHTML = `You Entered <span> ${values} </span> is Small than the actual number `;
-  } else {
-    alerts.classList.remove("hidealert");
-    alerts.appendChild(para).innerHTML = ` Hurray 🥇 you find Correct Number`;
-    alerts.appendChild(para2).innerHTML = `You got ${
-      10 - i
-    } marks out of ${10}`;
-    main.style.backgroundColor = "green";
-    displayValue.innerHTML = number;
-    setTimeout(() => {
-      location.reload();
-    }, 4000);
-  }
+  let values = parseInt(formValue.value);
+  if (i <= 10 && run) {
+    if (values > number) {
+      aside.appendChild(
+        para
+      ).innerHTML = `You Entered <span> ${values} </span> is Greater than the actual number `;
+      displayValue.innerHTML = values;
+    } else if (values < number) {
+      aside.appendChild(
+        para
+      ).innerHTML = `You Entered <span> ${values} </span> is Small than the actual number `;
+      displayValue.innerHTML = values;
+    } else {
+      run = false;
+      alerts.classList.remove("hidealert");
+      alerts.appendChild(para).innerHTML = ` Hurray 🥇 you find Correct Number`;
+      alerts.appendChild(para2).innerHTML = `You got ${
+        10 - i
+      } marks out of ${10}`;
+      main.style.backgroundColor = "green";
+      displayValue.innerHTML = number;
+    }
 
-  aside.appendChild(para1).innerHTML = `You tried <span> ${i} </span> times `;
-  i++;
-  console.log(i);
+    aside.appendChild(para1).innerHTML = `You tried <span> ${i} </span> times `;
+    i++;
+    console.log(i);
+  } else {
+    run = false;
+    aside.appendChild(para).innerHTML = `You Loss 😩`;
+    console.log(i);
+    main.style.backgroundColor = " rgb(255, 30, 30)";
+  }
 };
 
 const handleform = (e) => {
   e.preventDefault();
   increment();
 };
+X_mark.addEventListener("click", () => {
+  alerts.classList.add("hidealert");
+});
+again.addEventListener("click", () => {
+  displayValue.innerHTML = ``;
+  formValue.value = "";
+  aside.appendChild(para).innerHTML = "";
+  aside.appendChild(para1).innerHTML = "";
+  main.style.backgroundColor = "rgb(255, 137, 157)";
+  run = true;
+  i = 1;
+});
 
 formArea.addEventListener("submit", handleform);
